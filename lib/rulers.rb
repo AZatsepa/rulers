a5757be2f70ac4842ced0c274f0c9147c6873cbb
zatsepa <artem.zatsepa@nixsolutions.com>
2019-10-31 10:14:18 +0200

    Add controllers

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 require 'rulers/version'
+require 'rulers/array'
+require 'rulers/controller'
 require 'rulers/application'
+require 'pry'
 
 module Rulers
-  class Error < StandardError; end
-  # Your code goes here...
 end
--- lib/rulers/application.rb
+++ lib/rulers/application.rb
@@ -1,12 +1,30 @@
+# frozen_string_literal: true
+
+require 'rulers/controller'
+
 module Rulers
   class Application
-    def call(_env)
-      'echo debug > debug.txt'
-      [
-        200,
-        { 'Content-Type' => 'text/html' },
-        ['Hello from Ruby on Rulers!']
-      ]
+    def call(env)
+      return [404, { 'Content-Type' => 'text/html' }, []] if env['PATH_INFO'] == '/favicon.ico'
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.public_send(act)
+      [200, { 'Content-Type' => 'text/html' }, [text]]
+    rescue StandardError
+      [404, { 'Content-Type' => 'text/html' }, []]
+    end
+
+    def get_controller_and_action(env)
+      _, cont, action, _after =
+        env['PATH_INFO'].split('/', 4)
+      cont = cont.capitalize
+      if cont == ''
+        cont = 'Rulers::Controller'
+      else
+        cont += 'Controller'
+      end
+      [Object.const_get(cont), action.to_s]
     end
   end
 end
